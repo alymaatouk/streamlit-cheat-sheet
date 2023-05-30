c09d3bb78f6b1440065ec9789c074a61d1af8077
Ali Maatouk <115188345+alymaatouk@users.noreply.github.com>
2023-05-30 10:00:24 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-[![Open in Streamlit](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/master/app.py)
-
-# Streamlit Cheat Sheet
+"""
+Streamlit Cheat Sheet
 
 App to summarise streamlit docs v1.8.0
 
@@ -8,48 +7,109 @@ There is also an accompanying png and pdf version
 
 https://github.com/daniellewisDL/streamlit-cheat-sheet
 
-v1.8.0 April 2022
+v1.8.0 October 2021
 
 Author:
-* @daniellewisDL : https://github.com/daniellewisDL
+    @daniellewisDL : https://github.com/daniellewisDL
 
 Contributors:
-* @arnaudmiribel : https://github.com/arnaudmiribel
-* @akrolsmir : https://github.com/akrolsmir
-* @nathancarter : https://github.com/nathancarter
+    @arnaudmiribel : https://github.com/arnaudmiribel
+    @akrolsmir : https://github.com/akrolsmir
+    @nathancarter : https://github.com/nathancarter
 
-# Versioning
-* Based on Streamlit 1.8.0
-* Made with Python 3.8.5
+"""
 
-# Requirements
-A clean venv with just pip and then Streamlit
+import streamlit as st
+from pathlib import Path
+import base64
 
-# Deployments
-[Streamlit Cheat Sheet - Sharing for Streamlit](https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/master/app.py)
+# Initial page config
 
-[Streamlit Cheat Sheet - Heroku](https://streamlit-cheat-sheet.herokuapp.com/)
+st.set_page_config(
+     page_title='Streamlit cheat sheet',
+     layout="wide",
+     initial_sidebar_state="expanded",
+)
 
-# Show me
-![Streamlit Cheat Sheet](https://github.com/daniellewisDL/streamlit-cheat-sheet/blob/master/streamlit-cheat-sheet.png)
+def main():
+    cs_sidebar()
+    cs_body()
 
----
+    return None
 
-# Cheat sheet content
+# Thanks to streamlitopedia for the following code snippet
 
-## Magic commands
+def img_to_bytes(img_path):
+    img_bytes = Path(img_path).read_bytes()
+    encoded = base64.b64encode(img_bytes).decode()
+    return encoded
 
-```python
-# Magic commands implicitly `st.write()`
-''' _This_ is some __Markdown__ '''
+# sidebar
+
+def cs_sidebar():
+
+    st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=32 height=32>](https://streamlit.io/)'''.format(img_to_bytes("logomark_website.png")), unsafe_allow_html=True)
+    st.sidebar.header('Streamlit cheat sheet')
+
+    st.sidebar.markdown('''
+<small>Summary of the [docs](https://docs.streamlit.io/en/stable/api.html), as of [Streamlit v1.8.0](https://www.streamlit.io/).</small>
+    ''', unsafe_allow_html=True)
+
+    st.sidebar.markdown('__How to install and import__')
+
+    st.sidebar.code('$ pip install streamlit')
+
+    st.sidebar.markdown('Import convention')
+    st.sidebar.code('>>> import streamlit as st')
+
+    st.sidebar.markdown('__Add widgets to sidebar__')
+    st.sidebar.code('''
+st.sidebar.<widget>
+>>> a = st.sidebar.radio(\'R:\',[1,2])
+    ''')
+
+    st.sidebar.markdown('__Command line__')
+    st.sidebar.code('''
+$ streamlit --help
+$ streamlit run your_script.py
+$ streamlit hello
+$ streamlit config show
+$ streamlit cache clear
+$ streamlit docs
+$ streamlit --version
+    ''')
+
+    st.sidebar.markdown('__Pre-release features__')
+    st.sidebar.markdown('[Beta and experimental features](https://docs.streamlit.io/library/advanced-features/prerelease#beta-and-experimental-features)')
+    st.sidebar.code('''
+pip uninstall streamlit
+pip install streamlit-nightly --upgrade
+    ''')
+
+    st.sidebar.markdown('''<small>[st.cheat_sheet v1.8.0](https://github.com/daniellewisDL/streamlit-cheat-sheet)  | April 2022</small>''', unsafe_allow_html=True)
+
+    return None
+
+##########################
+# Main body of cheat sheet
+##########################
+
+def cs_body():
+    # Magic commands
+
+    col1, col2, col3 = st.columns(3)
+
+    col1.subheader('Magic commands')
+    col1.code('''# Magic commands implicitly `st.write()`
+\'\'\' _This_ is some __Markdown__ \'\'\'
 a=3
 'dataframe:', data
-```
+    ''')
 
+    # Display text
 
-## Display text
-
-```python
+    col1.subheader('Display text')
+    col1.code('''
 st.text('Fixed width text')
 st.markdown('_Markdown_') # see *
 st.caption('Balloons. Hundreds of them...')
@@ -62,22 +122,23 @@ st.subheader('My sub')
 st.code('for i in range(8): foo()')
 
 * optional kwarg unsafe_allow_html = True
-```
 
+    ''')
 
-## Display data
+    # Display data
 
-```python
+    col1.subheader('Display data')
+    col1.code('''
 st.dataframe(my_dataframe)
 st.table(data.iloc[0:10])
 st.json({'foo':'bar','fu':'ba'})
 st.metric(label="Temp", value="273 K", delta="1.2 K")
-```
+    ''')
 
+    # Display charts
 
-## Display charts
-
-```python
+    col1.subheader('Display charts')
+    col1.code('''
 st.line_chart(data)
 st.area_chart(data)
 st.bar_chart(data)
@@ -90,21 +151,21 @@ st.pydeck_chart(data)
 st.deck_gl_chart(data)
 st.graphviz_chart(data)
 st.map(data)
-```
+    ''')
 
+    # Display media
 
-## Display media
-
-```python
+    col1.subheader('Display media')
+    col1.code('''
 st.image('./header.png')
 st.audio(data)
 st.video(data)
-```
+    ''')
 
+    # Display interactive widgets
 
-## Display interactive widgets
-
-```python
+    col1.subheader('Display interactive widgets')
+    col1.code('''
 st.button('Hit me')
 st.download_button('On the dl', data)
 st.checkbox('Check me out')
@@ -121,27 +182,26 @@ st.time_input('Time entry')
 st.file_uploader('File uploader')
 st.camera_input("一二三,茄子!")
 st.color_picker('Pick a color')
-```
-
-### Use widgets' returned values in variables:
-
-```python
+    ''')
+    col1.write('Use widgets\' returned values in variables:')
+    col1.code('''
 >>> for i in range(int(st.number_input('Num:'))): foo()
 >>> if st.sidebar.selectbox('I:',['f']) == 'f': b()
 >>> my_slider_val = st.slider('Quinn Mallory', 1, 88)
 >>> st.write(slider_val)
-```
+    ''')
 
+    # Control flow
 
-## Control flow
-```python
+    col2.subheader('Control flow')
+    col2.code('''
 st.stop()
-```
+    ''')
 
+    # Lay out your app
 
-## Lay out your app
-
-```python
+    col2.subheader('Lay out your app')
+    col2.code('''
 st.form('my_form_identifier')
 st.form_submit_button('Submit to me')
 st.container()
@@ -151,30 +211,28 @@ st.columns(spec)
 st.expander('Expander')
 >>> with st.expander('Expand'):
 >>>     st.write('Juicy deets')
-```
+    ''')
 
-
-### Batch widgets together in a form:
-
-```python
+    col2.write('Batch widgets together in a form:')
+    col2.code('''
 >>> with st.form(key='my_form'):
 >>> 	text_input = st.text_input(label='Enter some text')
 >>> 	submit_button = st.form_submit_button(label='Submit')
-```
+    ''')
 
+    # Display code
 
-## Display code
-
-```python
+    col2.subheader('Display code')
+    col2.code('''
 st.echo()
 >>> with st.echo():
 >>>     st.write('Code will be executed and printed')
-```
+    ''')
 
+    # Display progress and status
 
-## Display progress and status
-
-```python
+    col2.subheader('Display progress and status')
+    col2.code('''
 >>> with st.spinner(text='In progress'):
 >>>   time.sleep(5)
 >>>   st.success('Done')
@@ -187,12 +245,12 @@ st.warning('Warning message')
 st.info('Info message')
 st.success('Success message')
 st.exception(e)
-```
+    ''')
 
+    # Placeholders, help, and options
 
-## Placeholders, help, and options
-
-```python
+    col2.subheader('Placeholders, help, and options')
+    col2.code('''
 # Replace any single element.
 >>> element = st.empty()
 >>> element.line_chart(...)
@@ -211,11 +269,12 @@ st.set_page_config(layout='wide')
 st.experimental_show(objects)
 st.experimental_get_query_params()
 st.experimental_set_query_params(**params)
-```
+    ''')
 
-## Mutate data
+    # Mutate data
 
-```python
+    col3.subheader('Mutate data')
+    col3.code('''
 # Add rows to a dataframe after
 # showing it.
 >>> element = st.dataframe(df1)
@@ -225,12 +284,13 @@ st.experimental_set_query_params(**params)
 # showing it.
 >>> element = st.line_chart(df1)
 >>> element.add_rows(df2)
-```
+    ''')
 
+    # Optimize performance
 
-## Optimize performance
-### Legacy caching
-```python
+    col3.subheader('Optimize performance')
+    col3.write('Legacy caching')
+    col3.code('''
 >>> @st.cache
 ... def foo(bar):
 ...   # Do something expensive in here...
@@ -242,10 +302,9 @@ st.experimental_set_query_params(**params)
 >>> d2 = foo(ref1)
 >>> # Different arg, so function foo executes
 >>> d3 = foo(ref2)
-```
-
-### Cache data objects
-```python
+    ''')
+    col3.write('Cache data objects')
+    col3.code('''
 >>> @st.experimental_memo
 ... def foo(bar):
 ...   # Do something expensive and return data
@@ -261,10 +320,9 @@ st.experimental_set_query_params(**params)
 >>> foo.clear()
 # Clear values from *all* memoized functions
 >>> st.experimental_memo.clear()
-```
-
-### Cache non-data objects
-```python
+    ''')
+    col3.write('Cache non-data objects')
+    col3.code('''
 # E.g. TensorFlow session, database connection, etc.
 >>> @st.experimental_singleton
 ... def foo(bar):
@@ -281,12 +339,19 @@ st.experimental_set_query_params(**params)
 >>> foo.clear()
 # Clear all singleton caches
 >>> st.experimental_singleton.clear()
-```
+    ''')
 
-### Other key parts of the API
+    col3.subheader('Other key parts of the API')
+    col3.markdown('''
 <small>[State API](https://docs.streamlit.io/en/stable/session_state_api.html)</small><br>
 <small>[Theme option reference](https://docs.streamlit.io/en/stable/theme_options.html)</small><br>
 <small>[Components API reference](https://docs.streamlit.io/en/stable/develop_streamlit_components.html)</small><br>
 <small>[API cheat sheet](https://share.streamlit.io/daniellewisdl/streamlit-cheat-sheet/app.py)</small><br>
+''', unsafe_allow_html=True)
 
----
+    return None
+
+# Run main()
+
+if __name__ == '__main__':
+    main()
